00faa2f2cc896959a0e0baf9bc31f1f40aa3b014
Cab <rpucholgimenez@gmail.com>
2025-05-23 13:23:04 +0200

    level 5 with home

--- Assets/Scripts/HomeScript.cs
+++ Assets/Scripts/HomeScript.cs
@@ -3,10 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class HomeScript : MonoBehaviour
 {
-    
-   public void Level1()
+
+    public void MainScene()
+    {
+        SceneManager.LoadSceneAsync("Main Scene");
+    }
+
+    public void Level1()
     {
         SceneManager.LoadSceneAsync("Level 1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadSceneAsync("Level 2");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadSceneAsync("Level 3");
+    }
+    public void Level4()
+    {
+        SceneManager.LoadSceneAsync("Level 4");
+    }
+    public void Level5()
+    {
+        SceneManager.LoadSceneAsync("Level 5");
     }
 
     
